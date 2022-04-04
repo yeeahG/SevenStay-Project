ec145eaa29df202471b7f66a26b15a4869244f25
yeeahG <yeji.sep.k@gmail.com>
2022-04-05 03:12:30 +0900

    [upload]login

--- front/src/components/LoginSignin/LoginForm.jsx
+++ front/src/components/LoginSignin/LoginForm.jsx
@@ -1,26 +1,27 @@
 import React, { useState, useUserContext} from 'react'
 import { useHistory } from 'react-router-dom';
+import UserLogin from './UserLogin'
 import './LoginForm.css'
 
 const LoginForm = () => {
   //내생각에 여기는 get method를 써서 db에서 등록된 id, pw랑 비교해서 button이 실행되게 해야할 듯
 
   //--------1버전
-  let [loginId, setLoginId] = useState("");
-  let [loginPassword, setLoginPassword] = useState("");
-  let [savedLoginId, setSavedLoginId] = useState("");
-  let [savedLoginPassword, setSavedLoginPassword] = useState("");
+  // let [loginId, setLoginId] = useState("");
+  // let [loginPassword, setLoginPassword] = useState("");
+  // let [savedLoginId, setSavedLoginId] = useState("");
+  // let [savedLoginPassword, setSavedLoginPassword] = useState("");
 
-  let sessionStorage = window.sessionStorage;
+  // let sessionStorage = window.sessionStorage;
 
-  const onSubmitAccount = () => {
-    sessionStorage.setItem("loginId", loginId);
-    sessionStorage.setItem("loginPassword", loginPassword);
+  // const onSubmitAccount = () => {
+  //   sessionStorage.setItem("loginId", loginId);
+  //   sessionStorage.setItem("loginPassword", loginPassword);
 
-    setSavedLoginId(sessionStorage.getItem("loginId"));
-    setSavedLoginPassword(sessionStorage.getItem("loginPassword"));
-    console.log(sessionStorage);
-  }
+  //   setSavedLoginId(sessionStorage.getItem("loginId"));
+  //   setSavedLoginPassword(sessionStorage.getItem("loginPassword"));
+  //   console.log(sessionStorage);
+  // }
 
   //-------2버전
   {/*const [userId, setUserId] = useState("");
@@ -34,9 +35,37 @@ const LoginForm = () => {
     } 
   } */}
 
+  //3버전
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin",
+  }
+  const [user, setUser]= useState({name: "", email:""});
+  const [error, setError]=useState("");
+
+  const Login = details => {
+    console.log(details);
+
+    if (details.email == adminUser.email && details.password == adminUser.password) {
+      console.log("logged in");
+      setUser({
+        name: details.name,
+        email: details.email
+    });
+    } else {
+    console.log("Details do not match");
+    setError("Details do not match")
+    }
+  } 
+
+  const Logout = () => {
+    console.log("log out");
+    setUser({name: "", email: ""});
+  }
+
   return (
     <div className='container'>
-       <input 
+       {/* <input 
         id="id" 
         name="id" 
         placeholder="아이디를 입력해주세요" 
@@ -63,13 +92,21 @@ const LoginForm = () => {
       
       <div>
         { JSON.stringify(sessionStorage) }
-      </div> 
+      </div>  */}
 
       {/* -------2버전 */}
       {/*<input value={userId} onChange={(e) => setUserId(e.target.value)} />
       <input value={password} onChange={(e) => setPassword(e.target.value)} />
       <button onClick={submit}>로그인</button>*/}
-     
+
+      {(user.email != "") ? (
+        <div className='welcome'>
+          <h2>Welcome, <span>{user.name}</span></h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      )  : (
+        <UserLogin Login={Login} error={error}/>
+      )}
     </div>
     
   )
